9a18db3b4de6e2a58f3fb92ec679fb2d5266d1d1
Richard GOZAN <gozamegborichard14@gmail.com>
2026-03-03 10:29:48 +0100

    First commit

---
+++ api.py
@@ -0,0 +1,17 @@
+import joblib
+from fastapi import FastAPI
+from main import predict as predict_name
+
+
+app = FastAPI()
+model = joblib.load("model.joblib")
+
+
+@app.get("/")
+async def root():
+    return {"status": "ok"}
+
+
+@app.get("/predict")
+async def predict(name: str):
+    return {"name": name, "sex": "M" if int(predict_name(model, name)[0]) == 1 else "F"}
